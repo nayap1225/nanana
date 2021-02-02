7320fd149e5f27f73d483284204ec2fdb8943104
nayap1225 <nayap1225@gmail.com>
2021-02-02 14:08:03 +0900

    animation

--- alertnow/assets/js/ui.js
+++ alertnow/assets/js/ui.js
@@ -1,18 +1,16 @@
 $(function(){
-	alertAni = setInterval(alertAniFn, 1000);
-	graphAni = setInterval(graphAniFn, 700);
-
 	dropdownFn('.h_dropdown .btn_toggle');
 	dropdownFn('.select_lang .btn_selected');
 
-
+	graphAniTimeFn()
+	alertAniTimeFn()
 });
 
 var alertAni = null;
 var graphAni = null;
 
 var _num1 = 0;
-function alertAniFn(el) {
+function alertAniFn() {
 	var _el = $('.animate_box .alert_box');
 	var _children = _el.children();
 	var _len = _children.length;
@@ -25,9 +23,15 @@ function alertAniFn(el) {
 		_num1 = 0;
 	};
 };
+function alertAniTimeFn(){
+    alertAniFn();
+	var chLen = $('.animate_box .alert_box').children().length;
+    var _speed = _num1 === 0 ? 500 : 1000; // 얼럿 애니메이션 인터벌 간격 :: 500 -> 사라졌다 나타나는 시간, 1000 :: 박스 나타나는 시간 (1000 = 1초)
+    setTimeout(alertAniTimeFn, _speed);
+};
 
 var _num2 = 0;
-function graphAniFn(el) {
+function graphAniFn() {
 	var _el = $('.animate_box .graph_box');
 	var _children = _el.children();
 	var _len = _children.length;
@@ -39,6 +43,13 @@ function graphAniFn(el) {
 		_el.children().removeClass('on')
 		_num2 = 0;
 	};
+};
+
+function graphAniTimeFn(){
+    graphAniFn();
+	var chLen = $('.animate_box .graph_box').children().length;
+    var _speed = _num2 === chLen ? 3000 : 700; // 그래프 애니메이션 인터벌 간격 -> 700 : 이미지 나타나는 시간, 2000 : 마지막 라인 이후 사라지기전 까지 시간 (1000 = 1초)
+    setTimeout(graphAniTimeFn, _speed);
 };
 
 function dropdownFn (el) {
